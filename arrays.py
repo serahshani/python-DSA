d274df8e34ca4ec679978fe96afe359497eece3d
serah shani <serahshani035@gmail.com>
2024-10-12 21:14:42 +0300

    arrays question and solution added

--- arrays.py
+++ arrays.py
@@ -65,4 +65,38 @@ def two_sum(arr, target):
     return False
 
 arr = [0, -1, 2, -3, 1]
-target = -2                              
+target = -2       
+
+#Given an array prices[] of length N, representing the prices of the stocks on different days, the task is to find the maximum profit possible by buying and selling the stocks on different days when at most one transaction is allowed. Here one transaction means 1 buy + 1 Sell.
+
+# Note: Stock must be bought before being sold.
+
+# Examples:
+
+# Input: prices[] = {7, 10, 1, 3, 6, 9, 2}
+# Output: 8
+# Explanation: Buy for price 1 and sell for price 9. 
+
+
+# Input: prices[] = {7, 6, 4, 3, 1} 
+# Output: 0
+# Explanation: Since the array is sorted in decreasing order, 0 profit can be made without making any transaction.
+
+# Input: prices[] = {1, 3, 6, 9, 11} 
+# Output: 10
+# Explanation: Since the array is sorted in increasing order, we can make maximum profit by buying at price[0] and selling at price[n-1]
+
+#naive approach
+def max_profit(prices):
+    n = len(prices)
+    res = 0
+    
+    #explore for all possible ways to buy and sell stock
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            res = max(res, prices[j] - prices[i])
+    return res
+if __name__ == "__main__":
+    prices = [7, 10, 1, 3, 6, 9, 2]
+    print(max_profit(prices))
+                               
